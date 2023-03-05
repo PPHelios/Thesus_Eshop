92156aedab9c687c4b53fa5d70fe5fdca1ecabff
PPHelios <pphelios1@gmail.com>
2023-03-05 18:44:30 +0200

    Refactor: refactored mui theme customiation

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,4 +1,25 @@
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Link from "@mui/material/Link";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <Box>
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+        Text only
+      </Typography>
+      <List>
+        <ListItem key={item.label} button component={Link} href={item.link}>
+          <ListItemText primary={t(`nav_bar.${item.label}`)} />
+        </ListItem>
+      </List>
+    </Box>
+  );
 }
 export default Footer;
