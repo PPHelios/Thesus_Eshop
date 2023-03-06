e8b274c50ee30edc320192fa9d066b2649d8ec4e
Haytham Atef <103271591+PPHelios@users.noreply.github.com>
2023-03-06 05:36:08 +0200

    Footer menus object

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,17 +8,61 @@ import ListItemText from "@mui/material/ListItemText";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import Link from "@mui/material/Link";
 
+const ourStoreLinks = [
+  { link: "/weekendBoots", label: "weekendBoots" },
+
+    { link: "/terraceClogs", label: "Terrace_Clogs" },
+
+    { link: "/accessories", label: "Accessories" },
+
+    { link: "/shopAll", label: "Shop_All" }
+]
+
+const helpLinks = [
+  {label:"sizeGuides", link:"/sizeGuides"},
+  {label:"shipping", link:"/shipping"},
+   {label:"refundPolicy", link:"/refundPolicy"},
+  {label:"faq", link:"/faq"},
+  ]
+const aboutUsLinks = [
+
+  {label:"values", link:"/values"},
+
+  {label:"terms", link:"/terms"},
+
+   {label:"contact", link:"/contact"},
+
+
+
+  ]
+
 function Footer() {
   return (
+    
+<Box>
     <Box>
-      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
-        Text only
+
+       
+
+       <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+
+        Our Store
+
       </Typography>
+      {ourStoreLinks.map(item => (
+       
       <List>
+
         <ListItem key={item.label} button component={Link} href={item.link}>
-          <ListItemText primary={t(`nav_bar.${item.label}`)} />
+
+          <ListItemText primary={t(`footer.${item.label}`)} />
+
         </ListItem>
+
       </List>
+      ))}
+       </Box>
+       
     </Box>
   );
 }
